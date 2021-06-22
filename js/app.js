d35295dbef7418e1d3526536c04ad62e0c2e8de7
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-06-22 03:53:08 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 'use strict'
+let username =prompt('To begin play with me ...what is your name ??')  
 
+alert('welcome to our website   '+ username + ' I think we will play afun game ')
 
+let correct = 0
 let answer = prompt('Do you think I studied at the University of Jordan?')
 
-function resultfun() {
+ let resultfun = function () {
+    let out ='' ;
     switch (answer) {
         case 'y':
         case 'Yes':
@@ -13,35 +17,129 @@ function resultfun() {
         case 'yeE':
         case 'yes':
         case 'YES':
+        case 'Y':
 
 
-            console.log('Great that is correct answer');
-            answer.toLowerCase()
+
+            out ='yes';
+            return out;
+            
             
 
             break;
         case 'No':
         case 'nO':
         case 'no':
+        case 'n':
+        case 'N':
+        case 'NO':
 
-         answer.toLowerCase()
+         out='no';
+         return out;
     
             break;
         default:
-            console.log('just answer yes or no');
+            
+            out= 'none define';
+            return out;
             break;
+           
     }
 }
-
-
-
 resultfun();
-console.log(answer);
+let result = resultfun();
+// console.log('you answer is  ' + result)
+ alert('you answer is  ' + result);
+ 
+ if (result == 'yes')  {
+    alert("That is not the correct answer you can look at my page and know more about me ")
+    
+   } else if(result == 'no') {
+     alert('Great that is the correct answer you can look at my page and know more about me')
+    correct=correct + 1 ;
+   } else if(result == 'none define') {
+    alert ('just answer yes or no  now you miss this qustion')
+    }
+
+
+    answer =prompt( 'DO  think I can drive a car??')  
+    resultfun();
+    result = resultfun();
+    // console.log('you answer is  ' + result)
+    alert('you answer is  ' + result);
+    if (result == 'yes')  {
+        
+        alert('Great that is the correct answer you can look at my page and know more about me')
+        correct=correct + 1 ;
+
+       } else if(result == 'no') {
+         alert("That is not the correct answer you can look at my page and know more about me ")
+       } else if(result == 'none define') {
+        alert ('just answer yes or no  now you miss this qustion') 
+        }
 
 
 
 
 
+
+        answer=prompt('Is the pizza my favoutit meal?')  
+        resultfun();
+         result = resultfun();
+         alert('you answer is  ' + result);
+         
+         if (result == 'yes')  {
+            alert("That is not the correct answer you can look at my page and know more about me ")
+            
+           } else if(result == 'no') {
+             alert('Great that is the correct answer you can look at my page and know more about me')
+            correct=correct + 1 ;
+           } else if(result == 'none define') {
+            alert ('just answer yes or no  now you miss this qustion')
+            }
+          
+            
+
+            answer=prompt('Do you think I cane write Poetry?')
+            resultfun();
+    result = resultfun();
+    alert('you answer is  ' + result);
+    if (result == 'yes')  {
+        
+        alert('Great that is the correct answer you can look at my page and know more about me')
+        correct=correct + 1 ;
+
+       } else if(result == 'no') {
+         alert("That is not the correct answer you can look at my page and know more about me ")
+       } else if(result == 'none define') {
+        alert ('just answer yes or no  now you miss this qustion') 
+        }
+
+
+        answer=prompt('Do you think I am good at cooking?')  
+        resultfun();
+        result = resultfun();
+        alert('you answer is  ' + result);
+        if (result == 'yes')  {
+            
+            alert('Great that is the correct answer you can look at my page and know more about me')
+            correct=correct + 1 ;
+    
+           } else if(result == 'no') {
+             alert("That is not the correct answer you can look at my page and know more about me ")
+           } else if(result == 'none define') {
+            alert ('just answer yes or no  now you miss this qustion') 
+            }
+
+            document.write('Oh dear I think it nice game you answer  ' + correct + ' qustions     ,       '    +  username + '  I think we will play again with another quastion' );
+            
+
+            
+
+
+
+
+ 
 
 
 
